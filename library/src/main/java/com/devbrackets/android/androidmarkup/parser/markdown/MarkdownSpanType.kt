ac5c367209dfce95837ce800f1db282bfafbc221
brianwernick <brianw@mediarain.com>
2016-02-20 09:55:57 -0700

    Started Architecture for a new MD parser that will correctly retain characters not contained in Spans

--- library/src/main/java/com/devbrackets/android/androidmarkup/parser/markdown/MarkdownSpanType.kt
+++ library/src/main/java/com/devbrackets/android/androidmarkup/parser/markdown/MarkdownSpanType.kt
@@ -3,10 +3,11 @@ package com.devbrackets.android.androidmarkup.parser.markdown
 import com.devbrackets.android.androidmarkup.parser.core.SpanType
 
 object MarkdownSpanType {
-    const val TEXT = SpanType.MAX_SPAN_TYPE +1
     const val UNKNOWN = SpanType.UNKNOWN
     const val BOLD = SpanType.BOLD
     const val ITALIC = SpanType.ITALIC
     const val ORDERED_LIST = SpanType.ORDERED_LIST
     const val UNORDERED_LIST = SpanType.UNORDERED_LIST
+    const val TEXT = SpanType.MAX_SPAN_TYPE +1
+    const val MAX_MARKDOWN_SPAN_TYPE = TEXT
 }
