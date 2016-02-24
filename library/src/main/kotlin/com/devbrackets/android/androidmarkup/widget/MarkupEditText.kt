73869ae981a28f91871466ce12e1dcc8e8f2556a
brianwernick <brianw@mediarain.com>
2016-02-24 15:54:16 -0700

    Removed the xml attribute as it was too limiting

--- library/src/main/kotlin/com/devbrackets/android/androidmarkup/widget/MarkupEditText.kt
+++ library/src/main/kotlin/com/devbrackets/android/androidmarkup/widget/MarkupEditText.kt
@@ -3,30 +3,22 @@ package com.devbrackets.android.androidmarkup.widget
 import android.content.Context
 import android.support.v7.widget.AppCompatEditText
 import android.util.AttributeSet
-import com.devbrackets.android.androidmarkup.R
 import com.devbrackets.android.androidmarkup.parser.core.MarkupParser
 import com.devbrackets.android.androidmarkup.parser.core.SpanType
 import com.devbrackets.android.androidmarkup.parser.html.HtmlParser
-import com.devbrackets.android.androidmarkup.parser.markdown.MarkdownParser
 
 /**
  * A WYSIWYG EditText for Markup languages such as HTML or
  * Markdown.  This leaves the UI up to the implementing application.
  */
 class MarkupEditText : AppCompatEditText {
-    lateinit var markupParser: MarkupParser
+    var markupParser: MarkupParser = HtmlParser()
 
-    constructor(context: Context) : super(context) {
-        init(context, null)
-    }
+    constructor(context: Context) : super(context) {}
 
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-        init(context, attrs)
-    }
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
 
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        init(context, attrs)
-    }
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {}
 
     fun toggleBold() {
         markupParser.updateSpan(text, SpanType.BOLD, selectionStart, selectionEnd)
@@ -50,41 +42,5 @@ class MarkupEditText : AppCompatEditText {
 
     fun setMarkup(markup: String) {
         setText(markupParser.toSpanned(markup))
-    }
-
-    protected fun init(context: Context, attrs: AttributeSet?) {
-        if (attrs == null || !readAttributes(context, attrs)) {
-            markupParser = HtmlParser()
-        }
-    }
-
-    /**
-     * Reads the attributes associated with this view, setting any values found
-
-     * @param context The context to retrieve the styled attributes with
-     * *
-     * @param attrs The [AttributeSet] to retrieve the values from
-     * *
-     * @return True if the attributes were read
-     */
-    protected fun readAttributes(context: Context, attrs: AttributeSet): Boolean {
-        if (isInEditMode) {
-            return false
-        }
-
-        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.MarkupTextView) ?: return false
-
-        //Updates the Parser
-        setParser(ParserAttr.get(typedArray.getInteger(R.styleable.MarkupTextView_parser, 0)))
-
-        typedArray.recycle()
-        return true
-    }
-
-    protected fun setParser(parserAttr: ParserAttr) {
-        when (parserAttr) {
-            ParserAttr.HTML -> markupParser = HtmlParser()
-            ParserAttr.MARKDOWN -> markupParser = MarkdownParser()
-        }
     }
 }
