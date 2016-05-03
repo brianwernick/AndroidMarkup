fd4e72ff183d12732f2ff5661334c1b27f9f43a9
brianwernick <brianw@mediarain.com>
2016-05-03 11:21:34 -0600

    Updated dependencies other minor changes

---
+++ library/src/main/kotlin/com/devbrackets/android/androidmarkup/text/style/UnderlineSpan.kt
@@ -0,0 +1,18 @@
+package com.devbrackets.android.androidmarkup.text.style
+
+import android.support.annotation.ColorInt
+import android.text.TextPaint
+import android.text.style.CharacterStyle
+import android.text.style.UpdateAppearance
+
+class UnderlineSpan(@ColorInt var color: Int, var thickness: Float = 1.0f) : CharacterStyle(), UpdateAppearance {
+    override fun updateDrawState(tp: TextPaint) {
+        try {
+            //NOTE: setUnderlineText is a public method, but is hidden in the SDK
+            val method = TextPaint::class.java.getMethod("setUnderlineText", Integer.TYPE, java.lang.Float.TYPE)
+            method.invoke(tp, color, thickness)
+        } catch (e: Exception) {
+            tp.isUnderlineText = true
+        }
+    }
+}
